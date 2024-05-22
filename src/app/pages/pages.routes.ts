8371323c31b2c6ebda4b4fa42cd69cfc7086eae4
Jailerwebwork <jailerwebwork@gmail.com>
2024-05-22 10:56:22 -0500

    primer commit

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { LayoutComponent } from '../layout/layout.component';
+
+export default [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [],
+  },
+] as Routes;
---
+++ src/app/pages/products-page/products-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './products-page.component.html',
+  styles: ``
+})
+export class ProductsPageComponent {
+
+}
